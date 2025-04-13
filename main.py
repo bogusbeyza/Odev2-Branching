9193090f313aa4294082634c5d5084ce28024a0c
Beyza Bogus <bbogusbeyza@gmail.com>
2025-04-13 23:47:30 +0300

    Feature branch için değişiklik yapıldı

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ veriler = [10, 20, 30, 40, 50]
 
 ortalama = sum(veriler) / len(veriler)
 print(f"Verilerin Ortalaması: {ortalama}")
+
+def cikarma(a, b):
+    return a - b
+
+print(f"Çıkarma Sonucu: {cikarma(sayi1, sayi2)}")
